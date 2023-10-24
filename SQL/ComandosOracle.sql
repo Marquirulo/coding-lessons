b8e2b85b6c4fc0a03d89475c8d3f5fb07baf41fd
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2023-10-24 09:14:26 +0200

    Update ComandosOracle.sql

--- SQL/ComandosOracle.sql
+++ SQL/ComandosOracle.sql
@@ -1,6 +1,11 @@
-/*--Tipos de datos--*/
+/*--TIPOS DE DATOS--*/
 
-/*Consultas*/
+
+/*--CONSULTAS--*/
 SELECT [* | campo1, campo2] *--Elijo los campos de la tabla que quiero ver (*) indica todos los campos
   FROM tabla1  *--Elijo de que tabla quiero hacer la consulta
 ORDER BY campo1 [DESC];
+
+/*--ALIAS--*/
+SELECT campo1 [AS] alias1, campo2 [AS] alias2
+  FROM tabla1;

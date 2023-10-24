164905231fc5cee35950997680224f6373d50a49
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2023-10-24 09:09:09 +0200

    Create ComandosOracle.sql

---
+++ SQL/ComandosOracle.sql
@@ -0,0 +1,6 @@
+/*--Tipos de datos--*/
+
+/*Consultas*/
+SELECT [* | campo1, campo2] *--Elijo los campos de la tabla que quiero ver (*) indica todos los campos
+  FROM tabla1  *--Elijo de que tabla quiero hacer la consulta
+ORDER BY campo1 [DESC];

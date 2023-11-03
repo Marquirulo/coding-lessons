50056983bdbe7f7a1963a4791a955d91acbe938f
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2023-11-03 09:37:35 +0100

    Update funcionesOracle.sql

--- SQL/funcionesOracle.sql
+++ SQL/funcionesOracle.sql
@@ -26,5 +26,58 @@ LENGTH(texto) -- Indica el tamaño del texto
 REVERSE(texto) -- Invierte el texto
 
 /*** FUNCIONES DE TRABAJO CON NULL ***/
-NVL(valor,sustituto) -- Si el valor es NULL devuelve sustituto
-NVL2(valor,sustituto1,sustituto2) -- Si el valor es NULL devuelve sustituto2, si no devuelve sustituto1
+NVL(valor,sustituto) -- Si el valor es NULL devuelve sustituto (Los sustitutos deben ser del mismo tipo de dato)
+NVL2(valor,sustituto1,sustituto2) -- Si el valor es NULL devuelve sustituto2, si no, devuelve sustituto1
+
+/*** FUNCIONES DE FECHA ***/
+SYSDATE -- Fecha y Hora actuales
+
+-- CALCULAR FECHAS --
+ADD_MONTHS(fecha, n) -- Añade/Sustrae n meses a la fecha
+MONTHS_BETWEEN(fecha1, fecha2) -- Diferencia en meses entre las dos fechas
+NEXT_DAY(fecha,dia) -- Indica el siguiente dia despues de esa fecha. dia = [Sábado|6]
+LAST_DAY(fecha) -- Ultimo dia del mes de esa fecha
+EXTRACT({day|month|year} FROM fecha) -- Extrae el valor indicado de la fecha indicada
+GREATEST(fecha1, fecha2,..) -- Saca la fecha más moderna
+LEAST(fecha1, fecha2,..) -- Saca la fecha más antigua
+ROUND|TRUNC(fecha [,'YEAR|MONTH|HH24|DAY']) -- Redondea|Trunca al valor de aplicar el formato
+
+/*** FUNCIONES DE CONVERSION ***/
+  /* modelodeFormato cumple la función de máscara
+     MÁSCARAS:
+       DDMMYYYY
+       DD/MM/YYY
+  */
+TO_CHAR(expresión [,modeloDeFormato])
+TO_NUMBER(expresión [,modeloDeFormato])
+TO_DATE(expresión [,modeloDeFormato])
+
+/*** FUNCIONES CONDICIONALES ***/
+-- CASE --
+SELECT nombre,
+  CASE cotizacion
+      WHEN 1 THEN salario*0.85
+      WHEN 2 THEN salario*0.93
+      WHEN 3 THEN salario*0.96
+      ELSE salario
+  END
+FROM empleados;
+/*Puede haber CASE complejos indicando  ops. lógicas*/
+SELECT nombre,
+  CASE 
+      WHEN nota>=5 THEN 'Suficiente'
+      WHEN nota>=5 THEN 'Notable'
+      WHEN nota>=5 THEN 'Sobresaliente'
+      ELSE 'Suspenso'
+  END AS calificacion
+FROM alumnos;
+
+-- DECODE --
+-- Permite simplificar los CASE SIMPLES
+SELECT nombre,
+DECODE(cotizacion,
+  1, salario*0.85,
+  2,salario * 0.93,
+  3,salario * 0.96,
+  salario)
+FROM empleados;

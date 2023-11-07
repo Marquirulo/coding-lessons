a527b7e586553b1d820643f70a7b26f1964829b9
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2023-11-07 10:28:40 +0100

    Create MultiplesTablasOracle.sql

---
+++ SQL/apuntes/visualizacion/MultiplesTablasOracle.sql
@@ -0,0 +1,9 @@
+/*PRODUCTO CARTESIANO*/ --Esto muestra todas las combinaciones posibles de varios campos indep. de la relación
+SELECT first_name, last_name, department_name
+    FROM employees, departments;
+
+/*TABLAS RELACIONADAS POR FK*/
+--Si quiero mostrar un campo que este en ambas tablas hay que ponerle delante la tabla a la que pertenece
+SELECT first_name, last_name, department_name, emp.department_id
+  FROM employees emp, departments dep -- Se puede poner alias también a las tablas para hacer referencias mas faciles                          
+WHERE emp.department_id = dep.department_id;

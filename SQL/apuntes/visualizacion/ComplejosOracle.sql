3139dd865e8ebea8302804199c47b32b31324749
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2023-11-10 09:24:47 +0100

    Update ComplejosOracle.sql

--- SQL/apuntes/visualizacion/ComplejosOracle.sql
+++ SQL/apuntes/visualizacion/ComplejosOracle.sql
@@ -2,7 +2,7 @@
 SELECT first_name, last_name, department_name
     FROM employees, departments;
 
-/*** TABLAS RELACIONADAS POR FK ***/
+/*** INNER JOIN (TABLAS RELACIONADAS POR FK) ***/
 -- Si quiero mostrar un campo que este en ambas tablas hay que ponerle delante la tabla a la que pertenece
 SELECT first_name, last_name, department_name, emp.department_id
   FROM employees emp, departments dep -- Se puede poner alias también a las tablas para hacer referencias mas faciles                          
@@ -10,5 +10,20 @@ WHERE emp.department_id = dep.department_id;
 
 /*** SELF JOIN (RELACIONES REFLEXIVAS) ***/
 select e.employee_id, e.last_name, j.employee_id, j.last_name
-from employees e, employees j    -- Hay que iniciar la tabla dos veces con alias
+from employees e, employees j    -- Hay que iniciar la tabla dos veces con distintos alias
 where e.manager_id = j.employee_id;
+
+/*** LEFT Y RIGHT JOIN (Todos los valores de la X tabla relacionandolos con la Y 
+                                                y si no tienen relación pone NULL) ***/
+/*El (+) indica cual es la tabla que se va a agregar (Y), es decir la que podrá dar NULL*/
+-- LEFT JOIN -- 
+SELECT e.apellido, d.dept_no, d.dnombre
+    FROM emple e, depart d
+WHERE e.dept_no = d.dept_no(+);
+
+-- RIGHT JOIN --
+SELECT e.apellido, d.dept_no, d.dnombre
+    FROM emple e, depart d
+WHERE e.dept_no(+) = d.dept_no;
+
+/*** SUBCONSULTAS ***/

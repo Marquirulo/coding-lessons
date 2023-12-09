9d35924a6c9dc47cb3197aa147994cb9396b5579
Miituberss <marcosbs32005@gmail.com>
2023-12-09 13:20:43 +0100

    d

--- Python/IA/Ejercicios_Basicos/Proyecto1.py
+++ Python/IA/Ejercicios_Basicos/Proyecto1.py
@@ -2,3 +2,15 @@
 '''
 Usaremos "Regresión" esto es una tecnica en la cual tu dispones de un gráfico XY X=VariableIndependiente Y=VariableDependiente
 '''
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#Importando datos
+temperatura_df = pd.read_csv("../Datos/Ejercicios_Basicos/Proyecto1.csv")
+
+#Visualizar
+sns.scatterplot(temperatura_df)

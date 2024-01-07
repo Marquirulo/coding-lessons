8c28419b4dbc8e8c72ed855060aa4b3564bd41fa
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-01-07 13:54:33 +0100

    Update Funciones.js

--- WEB/JavaScript/Apuntes/Funciones.js
+++ WEB/JavaScript/Apuntes/Funciones.js
@@ -2,15 +2,15 @@
   var edad = 20; //Podemos crear variables con valor,
   var dia, mes, calle; //O crear varias variables sin valor en una misma linea
   //Actualmente se recomiendan los siguientes terminos
-  let edad = 20 // Para variables
-  const nombre = 'Marcos' //Para constantes
+  let edad = 20; // Para variables
+  const nombre = 'Marcos'; //Para constantes
   //Para saber el tipo de variable podemos usar typeof
-  let num = 2
-  alert(typeof num) //OUTPUT: << number >>
-  alert(typeof nada) //OUTPUT: << undefined >>
+  let num = 2;
+  alert(typeof num); //OUTPUT: << number >>
+  alert(typeof nada); //OUTPUT: << undefined >>
 
 /**** ENTRADAS DE DATOS ****/
-  prompt('Introduce tu nombre: ', 'Nombre') //El primer parámetro indica el mesaje en pantalla y el segundo el valor por defecto
+  prompt('Introduce tu nombre: ', 'Nombre'); //El primer parámetro indica el mesaje en pantalla y el segundo el valor por defecto
 
 /**** OPERADORES ****
   *** Caracteres de escape ***            *** Operadores Aritméticos***

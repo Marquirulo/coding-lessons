55f0a30adf0f7f05c1a1b6e3b5218885a12b9f4e
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-01-12 09:23:57 +0100

    Create EliminarDatos.sql

---
+++ SQL/apuntes/modificacion(DML)/EliminarDatos.sql
@@ -0,0 +1,11 @@
+/*Para borrar registros de una tabla usamos DELETE*/
+/*Si intentamos borrar datos que son FK en otra tabla dará error de integridad referencial*/
+/*Lo anterior se puede solucionar con la opción de integridad al crear la tabla: 
+ON DELETE CASCADE
+  - NORMAL -> Se borran todos los registros asociados
+  - NULL -> los registros donde era FK convierten su valor en NULL
+  - POR DEFECTO -> los registros donde era FK convierten su valor en el indicado DF. */
+
+/**** DELETE ****/
+  DELETE FROM jobs
+  WHERE name = 'Himuro'; -- El WHERE puede utilizar subconsultas

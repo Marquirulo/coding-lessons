805ae944ad1253ba9eb3a58a447d1395e1a5895a
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-01-19 09:38:04 +0100

    Create creacionTablas.sql

---
+++ SQL/apuntes/creacion(DDL)/creacionTablas.sql
@@ -0,0 +1,40 @@
+/**** REGLAS ****
+● Deben comenzar con una letra
+● No deben tener más de 30 caracteres
+● Solo se permiten utilizar letras del alfabeto (inglés), números o el signo de subrayado (también los signos $ y #, pero esos se utilizan de manera especial, por lo que no son recomendados)
+● No puede haber dos tablas con el mismo nombre dentro del mismo esquema (pueden coincidir los nombres si están en distintos esquemas)
+● No puede coincidir con el nombre de una palabra reservada SQL (por ejemplo no se puede llamar SELECT a una tabla)
+● En el caso de que el nombre tenga espacios en blanco o caracteres nacionales (permitido solo en algunas bases de datos), entonces se suele entrecomillar con comillas dobles. En el estándar SQL 99 (respetado por Oracle) se pueden utilizar comillas dobles al poner el nombre de la tabla a fin de hacerla sensible a las mayúsculas (se diferenciará entre “FACTURAS” y “Facturas”). ¡¡MEJOR USAR _ !!
+****/
+
+/**** CREACION ****/
+    CREATE TABLE EMPLEADO (DNI            VARCHAR2(9),
+                           EDAD           NUMBER,
+                           COD_PROVINCIA  NUMBER(2),
+                           EXPEDIENTE     VARCHAR2(30),
+                           FECHA_CARGA    DATE);
+  --IDENTIFICAR PK--
+    CREATE TABLE EMPLEADO (DNI             VARCHAR2(9) PRIMARY KEY,
+                           EDAD            NUMBER,
+                           COD_PROVINCIA   NUMBER(2),
+                           EXPEDIENTE      VARCHAR2(30),
+                           FECHA_CARGA     DATE);
+  --NOT NULL--
+    CREATE TABLE EMPLEADO (DNI            VARCHAR2(9) PRIMARY KEY,
+                           EDAD           NUMBER NOT NULL,
+                           COD_PROVINCIA  NUMBER(2),
+                           EXPEDIENTE     VARCHAR2(30),
+                           FECHA_CARGA    DATE);
+  --UNIQUE--
+    CREATE TABLE EMPLEADO (DNI            VARCHAR2(9) PRIMARY KEY,
+                           EDAD           NUMBER NOT NULL,
+                           COD_PROVINCIA  NUMBER(2),
+                           EXPEDIENTE     VARCHAR2(30) UNIQUE,
+                           FECHA_CARGA    DATE);
+
+  --DEFAULT--
+    CREATE TABLE EMPLEADO (DNI            VARCHAR2(9) PRIMARY KEY,
+                           EDAD           NUMBER NOT NULL,
+                           COD_PROVINCIA  NUMBER(2),
+                           EXPEDIENTE     VARCHAR2(30) UNIQUE,
+                           FECHA_CARGA    DATE DEFAULT SYSDATE);

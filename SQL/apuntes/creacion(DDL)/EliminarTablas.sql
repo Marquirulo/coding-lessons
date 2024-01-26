15184a4cae743294ef7126bce8463df442cbd7e2
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-01-26 09:51:39 +0100

    Update EliminarTablas.sql

--- SQL/apuntes/creacion(DDL)/EliminarTablas.sql
+++ SQL/apuntes/creacion(DDL)/EliminarTablas.sql
@@ -1,2 +1,5 @@
 /**** DROP ****/
-  DROP TABLE provincias;
+  DROP TABLE provincias [PURGE]; --El PURGE simula una papelera
+
+/**** CASCADE ****/
+DROP TABLE provincias CASCADE CONSTRAINT; --Borra la tabla usando el cascade

1271700ab616395e133eb0b2e943f8c7d94981b8
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-01-26 08:59:22 +0100

    Update ejerciciosDDL(I).sql

--- SQL/ejercicios/ejerciciosDDL(I).sql
+++ SQL/ejercicios/ejerciciosDDL(I).sql
@@ -61,12 +61,12 @@ CREATE TABLE ejer8 AS SELECT dni iden, nombre usua, edad anos, fecha hoy FROM ej
 /*EJ9*/
 CREATE TABLE emple_depart AS
     SELECT e.apellido, d.dnombre
-    FROM emple e
+        FROM emple e
     JOIN depart d USING (dept_no);
 
 /*EJ10*/
 CREATE TABLE dept_ventas AS
     SELECT e.apellido, d.dnombre, sysdate fecha_alta
-    FROM emple e
+        FROM emple e
     JOIN depart d USING (dept_no)
     WHERE UPPER(d.dnombre) = 'VENTAS';

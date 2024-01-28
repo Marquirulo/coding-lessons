a4cba81cf1122a1c841cb423acb0689e992cc1b8
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-01-28 13:55:01 +0100

    Update Formularios.js

--- WEB/JavaScript/Apuntes/Formularios.js
+++ WEB/JavaScript/Apuntes/Formularios.js
@@ -32,3 +32,28 @@
         this.value ='Enviando...'; 
         this.form.submit();
     }
+//Podemos limitar el nº de caracteres en un elemento utilizando onkeypress
+    function limita(maximoCaracteres) {
+     var elemento = document.getElementById("texto");
+     if(elemento.value.length >= maximoCaracteres ) {
+         return false; // Al devolver false onkeypress no realizara su funcion normal (escribir un caracter)
+     } else {
+         return true;
+     }
+    }
+/*<textarea id="texto" onkeypress="return limita(100);"></textarea>*/
+//VALIDACIONES
+    //isNaN()
+        valor = document.getElementById("campo").value;
+        if (isNaN(valor)) { //Comprueba si el dato es un nº valido ("3") tambien lo entiende como numero
+            return false;
+        }
+    //select
+        indice = document.getElementById("opciones").selectedIndex;
+        if (indice == null || indice == 0) { //Comprueba que se haya seleccionado una opcion que no se la 1ª ya que la 1ª es "Seleccione una opcion"
+            return false;
+        }
+    //A un nivel más avanzado se pueden usar RegExp
+        regExp.test(valorAValidar)
+      //Ej: Comprueba que el formato sea +00 000000000 o +000 000000000
+        /^\+\d{2,3}\s\d{9}$/.test(valor) 

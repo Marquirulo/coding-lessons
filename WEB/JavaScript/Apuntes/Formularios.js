487e05e2ac809cec99d85042cbcd69bd9d93418d
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-01-30 11:47:13 +0100

    Update Formularios.js

--- WEB/JavaScript/Apuntes/Formularios.js
+++ WEB/JavaScript/Apuntes/Formularios.js
@@ -1,11 +1,12 @@
 //Hay tres maneras de acceder a los elementos de un formulario
     document.getElementById('nombre'); //Usando los metodos propios del DOM
-    document.froms[0].elements[0]; //Usando los Arrays de formulario
+    document.forms[0].elements[0]; //Usando los Arrays de formulario
     document.formulario; //El DOM permite acceder mediante el atributo name del formulario de esta manera
 
 //Para obtener el valor de los formularios usamos:
     //Texto y textarea
         let valor = document.getElementById("texto").value;
+
     //Radio y Checkbox
       //Aquí nos interesa conocer cual está seleccionado por lo cual usariamos el siguiente:
         let elementos = document.getElementsByName("pregunta");

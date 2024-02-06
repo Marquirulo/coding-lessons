2d4bdf57ac33d87d4a27bb8e352226a0b7a6595e
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-02-06 09:29:21 +0100

    Update Bloque1.sql

--- SQL/apuntes/PL/Bloque1.sql
+++ SQL/apuntes/PL/Bloque1.sql
@@ -1,8 +1,9 @@
 /* Tipos de datos:
-     Escalares: Almacenan un valor único.
+     Escalares: Nº, Caracteres, Fechas...
      Compuestos: Colecciones y registros
-     Referencias:
-     LOB:*/
+     Referencias: Punteros
+     LOB: Localizadores
+*/
 /*
 VARCHAR(n)
 NUMBER(p,e)
@@ -12,7 +13,23 @@ TIMESTAMP(p)
 */
 
 /**** VARIABLES ****/
+-- El operador de asinación es :=
 importe number(8,2); --Inicializar una variable (DF = NULL)
 contador number(2,0) := 0; -- Inicializar una variable con un valor
 nombre char(20) NOT NULL := 'Marcos'; -- Inicializar una variable con un valor y fuerzo que siempre tenga un valor
 eliminado BOOLEAN DEFAULT FALSE; -- Inicializar una variable con un falor por defecto
+
+/**** ATRIBUTOS ****/
+     /* %TYPE */
+     -- Permiten asignar el tipo de una variable referenciada a un campo o variable ya existente
+          n_dept depart.cod_dept%TYPE;
+     /* %ROWTYPE */
+          
+/**** BLOQUE ****/
+     DECLARE IS / AS
+          [declaraciones] -- Definimos todos los objetos
+     BEGIN
+          [instrucciones] -- Conteiene las sentencias ejecutables de PL/SQL
+     EXCEPTION
+          [tratamiento de excepciones] -- Tratamiento de errores (Como en Python)
+     END;

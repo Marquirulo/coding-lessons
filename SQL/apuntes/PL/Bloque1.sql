fbcbe94576c8d784e2b19107dfad6d17b7b01e83
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-02-06 09:18:47 +0100

    Update Bloque1.sql

--- SQL/apuntes/PL/Bloque1.sql
+++ SQL/apuntes/PL/Bloque1.sql
@@ -1,1 +1,18 @@
+/* Tipos de datos:
+     Escalares: Almacenan un valor único.
+     Compuestos: Colecciones y registros
+     Referencias:
+     LOB:*/
+/*
+VARCHAR(n)
+NUMBER(p,e)
+BOOLEAN(TRUE|FALSE|NULL)
+DATE
+TIMESTAMP(p)
+*/
 
+/**** VARIABLES ****/
+importe number(8,2); --Inicializar una variable (DF = NULL)
+contador number(2,0) := 0; -- Inicializar una variable con un valor
+nombre char(20) NOT NULL := 'Marcos'; -- Inicializar una variable con un valor y fuerzo que siempre tenga un valor
+eliminado BOOLEAN DEFAULT FALSE; -- Inicializar una variable con un falor por defecto

d362c837ea27ab60e2f8df3d386d64e3bae6ec65
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-02-09 09:23:59 +0100

    Update Bloque1.sql

--- SQL/apuntes/PL/Bloque1.sql
+++ SQL/apuntes/PL/Bloque1.sql
@@ -22,6 +22,7 @@ eliminado BOOLEAN DEFAULT FALSE; -- Inicializar una variable con un falor por de
 
 /**** CURSORES ****/
 -- En PL, los resultados de consultas no se muestrasn en pantalla, sino que se almacenan en una area de memoria a parte
+
      /* IMPLICITOS */
      -- Se usan para operaciones SELECT INTO, es decir cuando la consulta devuelve solo un registro
      /*
@@ -38,8 +39,10 @@ eliminado BOOLEAN DEFAULT FALSE; -- Inicializar una variable con un falor por de
                FROM emple
           WHERE emp_no = 7782;
      END;
+
      /* EXPLICITOS */
      -- Son declarados y controlados por el programador.
+
 /**** ATRIBUTOS ****/
      /* %TYPE */
      -- Permiten asignar el tipo de una variable referenciada a un campo o variable ya existente
@@ -49,8 +52,10 @@ eliminado BOOLEAN DEFAULT FALSE; -- Inicializar una variable con un falor por de
      /*%ROWCOUNT*/
      -- Cuenta registros
           SQL%ROWCOUNT -- Cuenta los registros afectados por al ultima sentencia DML *ver SQL* 
+               
 /**** SQL ****/
      SQL -- Hace referencia a la ultima sentencia DML realizada
+               
 /**** BLOQUE DE LOS PROCEDIMIENTOS ****/
  /* BLOQUE ANÓNIMO */
      DECLARE [IS | AS] -- OPCIONAL
@@ -81,3 +86,28 @@ eliminado BOOLEAN DEFAULT FALSE; -- Inicializar una variable con un falor por de
      SET SERVEROUTPUT ON; -- Activa la salida por pantalla
      DBMS_OUTPUT.PUT_LINE('Hola ' || nombre);
      DBMS_OUTPUT.PUT_LINE('Error en la aplicación ' || SQLERRM);
+
+/**** EXCEPCIONES ****/
+-- Al igual que en otros lenguajes permite controlar los errores
+EXCEPTION
+     WHEN error_perso_1 THEN -- Definida por el usuario
+          RAISE_APPLICATION_ERROR([-20001 a -20999],'Mensaje de error personalizado'); -- Creas un error indicado un nº ORA entre 20001 y 20999
+     WHEN mirar_tabla_debajo THEN -- Definida por el sistema
+          DBMS_OUTPUT.PUT_LINE('SQLCODE: ' || SQLCODE); -- SQLCODE muestra el código que ha fallado
+          DBMS_OUTPUT.PUT_LINE('SQLERRM: ' || SQLERRM); -- SQLERRM Devuelve el código de error especifico.
+     WHEN OTHERS THEN -- Indicaciones para el resto de errores
+          /*Este código permite crear una tabla como log de errores*/
+          err_code := SQLCODE;
+          err_msg := SUBSTR(SQLERRM, 1, 200);
+          INSERT INTO log_table (error_number, error_message)
+          VALUES (err_code, err_msg);
+-- Excepciones comunes
+     TOO_MANY_ROWS    -- Se produce cuando SELECT xxx INTO devuelve más de una fila.
+     NO_DATA_FOUND    -- Se produce cuando SELECT xxx INTO no devuelve ninguna fila.
+     LOGIN_DENIED     -- Error cuando intentamos conectarnos a Oracle con un login y clave no validos.
+     NOT_LOGGED_ON    -- Se produce cuando intentamos acceder a la base de datos sin estar conectados.
+     PROGRAM_ERROR    -- Se produce cuando hay un problema interno en la ejecución del programa.
+     VALUE_ERROR      -- Se produce cuando hay un error aritmético o de conversión.
+     ZERO_DIVIDE      -- Se puede cuando hay una división entre 0.
+     DUPVAL_ON_INDEX  -- Se crea cuando se intenta almacenar un valor que crearía duplicados en la clave primaria o en una columna con restricción UNIQUE.
+     INVALID_NUMBER   -- Se produce cuando se intenta convertir una cadena a un valor numérico.

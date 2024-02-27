22ce318ad82f456b0d0bc8b2dddf7c97ce8024c7
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-02-27 09:32:41 +0100

    Update EJERCICIOS_PLSQL(III).sql

--- SQL/ejercicios/EJERCICIOS_PLSQL(III).sql
+++ SQL/ejercicios/EJERCICIOS_PLSQL(III).sql
@@ -1,1 +1,35 @@
+SET SERVEROUTPUT ON;
+/*Ej1*/
+DECLARE
+    num1 NUMBER(3);
+    num2 NUMBER(3);
+BEGIN
+    num1 := &num;
+    num2 := &num;
+    
+    IF (num1 > num2) THEN
+        num1 := num1+1;
+        num2 := num2+1;
+        FOR i IN num1..num2 LOOP
+            DBMS_OUTPUT.PUT_LINE(i);
+        END LOOP;
+    ELSE
+        num1 := num1+1;
+        num2 := num2+1;
+        FOR i IN num2..num1 LOOP
+            DBMS_OUTPUT.PUT_LINE(i);
+        END LOOP;
+    END IF;
+END;
+/
 
+/*Ej2*/
+DECLARE
+  num NUMBER(4);
+BEGIN
+  IF (MOD(num, 2) = 0) THEN
+    DBMS_OUTPUT.PUT_LINE('El numero ' || num || ' es par');
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('El numero ' || num || ' es impar');
+  END IF;
+END;

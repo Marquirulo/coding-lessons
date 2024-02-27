5da5b29c358f02903cf0e2c3464eff16064cda6b
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-02-27 09:18:50 +0100

    Update Bloque2.sql

--- SQL/apuntes/PL/Bloque2.sql
+++ SQL/apuntes/PL/Bloque2.sql
@@ -1,1 +1,38 @@
+-- TERCER TRIMESTRE --
+/**** BUCLES ****/
+  /*WHILE*/
+    WHILE (<condicion>) LOOP
+        ejecuciones;
+    END LOOP;
+  -- EJEMPLO --
+    DECLARE
+      num NUMBER(3);
+      i NUMBER(3);
+    BEGIN
+      num := &numero;
+      i :=1; --inicializamos
+      WHILE (i <= num) LOOP
+        INSERT INTO emple (emp_no) VALUES (i); --instrucciones
+        i := i+1; --variación de la condición
+      END LOOP;
+      COMMIT;
+    END;
+    /
 
+  /*FOR LOOP*/
+  --Contador no se declara en el DECLARE.
+  --IN hace el FOR normal, IN REVERSE a la inversa. 
+    FOR contador { IN | IN REVERSE } limite_inf..limite_sup LOOP 
+        ejecuciones
+    END LOOP;
+
+  -- EJEMPLO --
+    DECLARE
+      suma number:=0;
+    BEGIN
+      FOR variable IN 1..10 LOOP
+        suma := suma + variable;
+        dbms_output.put_line(suma);
+      END LOOP;
+    END;
+    /

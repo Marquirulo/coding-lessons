3f4ea80ff9c0ee9402291d8060cb32fbb4c4d92d
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-03-19 09:20:24 +0100

    Update and rename Ejercicios(V).sql to EjerciciosPL(V).sql

---
+++ SQL/ejercicios/EjerciciosPL(V).sql
@@ -0,0 +1,133 @@
+/*Ej1*/
+CREATE OR REPLACE PROCEDURE ejercicio1(codigo productos.cod_producto%TYPE)
+AS
+    precio productos.precio_uni%TYPE;
+BEGIN
+    SELECT precio_uni INTO precio
+        FROM productos
+    WHERE cod_producto = codigo;
+    DBMS_OUTPUT.PUT_LINE('El producto '||codigo||' cuesta '||precio);
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Error' || SQLERRM);
+END;
+/
+
+/*Ej2*/
+CREATE OR REPLACE PROCEDURE ejercicio2(codigo productos.cod_producto%TYPE, precio productos.precio_uni%TYPE)
+AS
+    v_precio productos.precio_uni%TYPE;
+BEGIN
+    SELECT precio_uni INTO v_precio
+        FROM productos
+    WHERE cod_producto = codigo;
+   
+    IF (ABS(precio - v_precio) > v_precio*0.2) THEN
+            DBMS_OUTPUT.PUT_LINE('La variacion del precio supera el 20%');
+    ELSE
+        UPDATE productos
+            SET precio_uni = precio
+        WHERE cod_producto = codigo;
+        DBMS_OUTPUT.PUT_LINE('Producto modificado correctamente');
+    END IF;
+EXCEPTION
+    WHEN no_data_found THEN
+        DBMS_OUTPUT.PUT_LINE('El producto no existe');
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error: '||SQLERRM);
+END;
+/
+
+/*Ej3*/
+CREATE OR REPLACE PROCEDURE ejercicio3(p_num_emple emple.emp_no%TYPE, p_nuevo_oficio emple.oficio%TYPE)
+AS
+    v_oficio_antiguo emple.oficio%TYPE;
+BEGIN
+    SELECT oficio INTO v_oficio_antiguo
+        FROM emple
+    WHERE emp_no = p_num_emple;
+    
+    UPDATE emple
+        SET oficio = p_nuevo_oficio
+    WHERE emp_no = p_num_emple;
+    
+    DBMS_OUTPUT.PUT_LINE('Oficio del empleado '|| p_num_emple || ' de ' || v_oficio_antiguo || ' a ' ||p_nuevo_oficio);
+    
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error: '||SQLERRM);
+END;
+/
+
+/*Ej4*/
+CREATE OR REPLACE PROCEDURE ejercicio4(p_nuevo_oficio emple.oficio%TYPE)
+AS
+    v_emple_max emple.emp_no%TYPE;
+BEGIN
+
+    SELECT MAX(emp_no) INTO v_emple_max
+        FROM emple;
+
+    ejercicio3(v_emple_max, p_nuevo_oficio);
+EXCEPTION
+    WHEN no_data_found THEN
+        DBMS_OUTPUT.PUT_LINE('El producto no existe');
+END;
+/
+
+/*Ej5*/
+CREATE OR REPLACE FUNCTION ejercicio5(p_apellido emple.apellido%TYPE)
+RETURN emple.emp_no%TYPE AS
+    v_emp_no emple.emp_no%TYPE;
+BEGIN
+    SELECT emp_no INTO v_emp_no
+        FROM emple
+    WHERE apellido = p_apellido;
+    DBMS_OUTPUT.PUT_LINE('El empleado con apellido ' || p_apellido ||' tiene el nº de empleado '||v_emp_no);
+    RETURN v_emp_no;
+EXCEPTION
+    WHEN no_data_found THEN
+        DBMS_OUTPUT.PUT_LINE('No exiten empleados con el apellido '|| p_apellido);
+        RETURN -1;
+        
+    WHEN too_many_rows THEN
+        DBMS_OUTPUT.PUT_LINE('Hay varios empleados con el apellido '|| p_apellido);
+        RETURN -2;
+        
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error '|| SQLERRM);
+        RETURN -3;
+        
+END;
+/
+
+/*Ej6*/
+CREATE OR REPLACE PROCEDURE subirSalario(p_emp_no emple.emp_no%TYPE, p_incremento NUMBER)
+AS
+    v_salario emple.salario%TYPE;
+    v_nuevo_salario emple.salario%TYPE;
+BEGIN
+
+    SELECT salario INTO v_salario
+        FROM emple
+    WHERE emp_no = p_emp_no;
+    
+    v_nuevo_salario := v_salario+p_incremento;
+    
+    IF (v_salario IS NULL) THEN
+        UPDATE emple
+            SET salario = p_incremento
+        WHERE emp_no = p_emp_no;
+        DBMS_OUTPUT.PUT_LINE('Salario de '||p_emp_no||' pasó de NULL a '||p_incremento);
+    ELSE 
+        UPDATE emple
+            SET salario = v_nuevo_salario
+        WHERE emp_no = p_emp_no;
+        DBMS_OUTPUT.PUT_LINE('Salario de '||p_emp_no||' pasó de '||v_salario||' a '||v_nuevo_salario);
+    END IF;
+    
+EXCEPTION
+    WHEN no_data_found THEN
+        DBMS_OUTPUT.PUT_LINE('El empleado indicado no existe');
+END;
+/

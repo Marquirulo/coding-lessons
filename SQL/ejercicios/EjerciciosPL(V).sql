85a088e7b742d5963675fc5f9f9f27845e681209
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-03-19 10:59:57 +0100

    Update EjerciciosPL(V).sql

--- SQL/ejercicios/EjerciciosPL(V).sql
+++ SQL/ejercicios/EjerciciosPL(V).sql
@@ -131,3 +131,90 @@ EXCEPTION
         DBMS_OUTPUT.PUT_LINE('El empleado indicado no existe');
 END;
 /
+
+/*Ej7*/
+CREATE OR REPLACE PROCEDURE subirSalario(p_emp_no emple.emp_no%TYPE, p_incremento NUMBER)
+AS
+    v_salario emple.salario%TYPE;
+    v_nuevo_salario emple.salario%TYPE;
+BEGIN
+
+    SELECT salario INTO v_salario
+        FROM emple
+    WHERE emp_no = p_emp_no;
+    
+    v_nuevo_salario := v_salario+p_incremento;
+    
+    IF (v_salario IS NULL) THEN
+        UPDATE emple
+            SET salario = p_incremento
+        WHERE emp_no = p_emp_no;
+        RAISE_APPLICATION_ERROR(-20010, 'El salario del empleado indicado es nulo');
+    ELSE 
+        UPDATE emple
+            SET salario = v_nuevo_salario
+        WHERE emp_no = p_emp_no;
+        DBMS_OUTPUT.PUT_LINE('Salario de '||p_emp_no||' pasó de '||v_salario||' a '||v_nuevo_salario);
+    END IF;
+    
+EXCEPTION
+    WHEN no_data_found THEN
+        DBMS_OUTPUT.PUT_LINE('El empleado indicado no existe');
+END;
+/
+
+/*Ej8*/
+--8.1 (Opcion1)
+CREATE OR REPLACE PROCEDURE 
+    insert_depart(p_nombre depart.dnombre%TYPE, p_localidad depart.loc%TYPE)
+AS
+    v_nombre depart.dnombre%TYPE;
+    v_dept_no depart.dept_no%TYPE;
+BEGIN
+    SELECT dnombre INTO v_nombre
+        FROM depart
+    WHERE UPPER(dnombre) = UPPER(p_nombre);
+    DBMS_OUTPUT.PUT_LINE('Ya existe un departamento con ese nombre');
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        SELECT MAX(dept_no)+10 INTO v_dept_no
+            FROM depart;
+        IF (v_dept_no IS NULL) THEN
+            INSERT INTO depart VALUES (10, UPPER(p_nombre), UPPER(p_localidad));
+            DBMS_OUTPUT.PUT_LINE('Nuevo departamento añadido con los siguientes datos 10 '||UPPER(p_nombre)||' '||UPPER(p_localidad));
+        ELSE
+            INSERT INTO depart VALUES (v_dept_no, UPPER(p_nombre), UPPER(p_localidad));
+            DBMS_OUTPUT.PUT_LINE('Nuevo departamento añadido con los siguientes datos '||v_dept_no||' '||p_nombre||' '||p_localidad);
+        END IF;
+END;
+/
+--8.1 (Opcion2)
+CREATE OR REPLACE PROCEDURE 
+    insert_depart(p_nombre depart.dnombre%TYPE, p_localidad depart.loc%TYPE)
+AS
+    v_nombre NUMBER(2);
+    v_dept_no depart.dept_no%TYPE;
+BEGIN
+    SELECT COUNT(1) INTO v_nombre
+        FROM depart
+    WHERE UPPER(dnombre) = UPPER(p_nombre);
+
+    SELECT MAX(dept_no)+10 INTO v_dept_no
+            FROM depart;
+
+    IF (v_nombre = 0 AND v_dept_no IS NULL) THEN
+        INSERT INTO depart VALUES (10, UPPER(p_nombre), UPPER(p_localidad));
+        DBMS_OUTPUT.PUT_LINE('Nuevo departamento añadido con los siguientes datos 10 '||UPPER(p_nombre)||' '||UPPER(p_localidad));
+    ELSIF (v_nombre = 0 AND v_dept_no IS NOT NULL)
+        INSERT INTO depart VALUES (v_dept_no, UPPER(p_nombre), UPPER(p_localidad));
+        DBMS_OUTPUT.PUT_LINE('Nuevo departamento añadido con los siguientes datos '||v_dept_no||' '||p_nombre||' '||p_localidad);
+    ELSE
+        RAISE DUP_VAL_ON_INDEX;
+    END IF;
+    
+    
+EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN
+        DBMS_OUTPUT.PUT_LINE('Ya existe un departamento con ese nombre');
+END;
+/

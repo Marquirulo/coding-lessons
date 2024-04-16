6935c43d854b40cf1c8a58ad19886653283a9c48
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-04-16 10:35:34 +0200

    Update MongoDB.sql

--- SQL/apuntes/NoSQL/MongoDB.sql
+++ SQL/apuntes/NoSQL/MongoDB.sql
@@ -1,1 +1,10 @@
+/*
+----- ARQUITECTURA --> BBDD{ COLECCION{ DOCUMENTOS{ _ID } } } -----
+«_id» es un campo obligatorio para cada documento. Representa un valor único y puede considerarse como la clave principal del documento para identificarlo dentro de la colección.
 
+Un documento es el equivalente a un registro en una base de datos tradicional. Se compone de campos de nombre y valor. Cada campo es una asociación entre un nombre y un valor y es similar a una columna en una base de datos relacional.
+
+Una colección es un grupo de documentos de MongoDB, y se corresponde con una tabla creada con cualquier otro RDBMS como Oracle en una base de datos relacional. No tiene una estructura predefinida.
+
+Una base de datos es un contenedor de colecciones, al igual que un RDBMS es un contenedor de tablas para las bases de datos relacionales. Cada uno tiene su propio conjunto de archivos en el sistema de archivos. Un servidor MongoDB puede almacenar múltiples bases de datos.
+*/
